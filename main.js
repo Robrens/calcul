0ec3842a2a3b299ee79005169a997c12b3d7a64b
PRUNIERES <aurelie.prunieres@gmail.com>
2018-05-28 15:00:52 +0200

    vol audessus d'un nid de coucou

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-var one= document.getElementsByName('button1');
+var screen = document.getElementById('text');
+var one = document.getElementById('button1');
+console.log(one);
+// var disOne = one.value;
 
-one.addEventListener('clic', function(){
-    one.value
-})
+
+one.addEventListener('click', calcul1);
+
+function calcul1(){
+   alert("hello");
+}
+
