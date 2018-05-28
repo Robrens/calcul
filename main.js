03d4f82571771c683236183914e11af280f6d7c6
Robrens <jbb.baudu@wanadoo.fr>
2018-05-28 15:53:00 +0200

    one button is working youhou !

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
 var screen = document.getElementById('text');
 var one = document.getElementById('button1');
-console.log(one);
-// var disOne = one.value;
+var plus = document.getElementById('button+');
+var two = document.getElementById('button2');
+var disOne = one.value;
+var disPlus = plus.value;
+var disTwo = two.value;
 
+// one.addEventListener('click', calcul1);
+//
+// function calcul1(){
+// screen.value = disOne
+// }
 
-one.addEventListener('click', calcul1);
+function verification(entree) {
+  var car ="1234567890[]()+-.*,/";
+  for (var i = 0; i < entree.length; i++)
+   if (car.indexOf(entree.charAt(i))<0 ) return false;
+  return true;
+ }
 
-function calcul1(){
-   alert("hello");
+ function calcul() {
+   var a = 0;
+  if (verification(window.document.calculatrice.result.value))
+     a = eval(window.document.calculatrice.result.value);
+   window.document.calculatrice.result.value = a;
+ }
+
+ function ajouter(caracteres) {
+   window.document.calculatrice.result.value =
+   window.document.calculatrice.result.value + caracteres;
+ }
+
+ function fonction_speciale(fonction) {
+   if (verification(window.document.calculatrice.result.value)) {
+     if(fonction == "sqrt") {
+       var a = 0;
+     a = eval(window.document.calculatrice.result.value);
+     window.document.calculatrice.result.value = Math.sqrt(a);
+   }
+   if(fonction == "pow") {
+     var a = 0;
+     a = eval(window.document.calculatrice.result.value);
+     window.document.calculatrice.result.value = a * a;
+   }
+   if(fonction == "log") {
+     var a = 0;
+     a = eval(window.document.calculatrice.result.value);
+     window.document.calculatrice.result.value = Math.log(a);
+   }
+  } else window.document.calculatrice.result.value = 0
 }
-
