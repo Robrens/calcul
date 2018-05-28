9425259a5b4b05c8e73af118619658e48a81c2a6
PRUNIERES <aurelie.prunieres@gmail.com>
2018-05-28 16:39:21 +0200

    mise en place de la fonction

--- main.js
+++ main.js
@@ -1,52 +1,19 @@
 var screen = document.getElementById('text');
-var one = document.getElementById('button1');
-var plus = document.getElementById('button+');
-var two = document.getElementById('button2');
-var disOne = one.value;
-var disPlus = plus.value;
-var disTwo = two.value;
+var fButton = document.getElementsByClassName('button');
+function calcul(){
+	screen.value = this.value;
+	screen.value = 
 
-// one.addEventListener('click', calcul1);
-//
-// function calcul1(){
-// screen.value = disOne
+}
+
+for (i=0; i< fButton.length; i++){
+	fButton[i].addEventListener("click", calcul);
+}
+	
+
+
+// fButton.addEventListener("click", calcul);
+// function calcul(){
+// 	console.log("hello")
 // }
 
-function verification(entree) {
-  var car ="1234567890[]()+-.*,/";
-  for (var i = 0; i < entree.length; i++)
-   if (car.indexOf(entree.charAt(i))<0 ) return false;
-  return true;
- }
-
- function calcul() {
-   var a = 0;
-  if (verification(window.document.calculatrice.result.value))
-     a = eval(window.document.calculatrice.result.value);
-   window.document.calculatrice.result.value = a;
- }
-
- function ajouter(caracteres) {
-   window.document.calculatrice.result.value =
-   window.document.calculatrice.result.value + caracteres;
- }
-
- function fonction_speciale(fonction) {
-   if (verification(window.document.calculatrice.result.value)) {
-     if(fonction == "sqrt") {
-       var a = 0;
-     a = eval(window.document.calculatrice.result.value);
-     window.document.calculatrice.result.value = Math.sqrt(a);
-   }
-   if(fonction == "pow") {
-     var a = 0;
-     a = eval(window.document.calculatrice.result.value);
-     window.document.calculatrice.result.value = a * a;
-   }
-   if(fonction == "log") {
-     var a = 0;
-     a = eval(window.document.calculatrice.result.value);
-     window.document.calculatrice.result.value = Math.log(a);
-   }
-  } else window.document.calculatrice.result.value = 0
-}
